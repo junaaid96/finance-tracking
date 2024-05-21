27ccf91660bfe3a597a3d315c65711b91d01aa83
junaaid96 <info.junaidulislam@gmail.com>
2024-05-21 15:50:55 +0600

    settings updated

--- finance_tracking/settings.py
+++ finance_tracking/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://finance-tracking-drf.onrender.com', 'https://*.127.0.0.1']
 
 
 # Application definition
